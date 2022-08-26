fd804f902f2082012fbb8ec4153d1b69ebfaa6e5
André Ferreira <contato@andreferreira.me>
2022-08-26 13:45:53 +0100

    Add getBlockHash method and some refactors

--- libs/bitcoin-core/src/bitcoin-core.service.ts
+++ libs/bitcoin-core/src/bitcoin-core.service.ts
@@ -1,9 +1,9 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, Logger } from '@nestjs/common';
+import { HttpException, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AxiosResponse } from 'axios';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
@@ -19,34 +19,46 @@ export class BitcoinCoreService {
     return this.rpcRequest('getblockchaininfo');
   }
 
+  getBlockHash(height: number): Observable<any> {
+    return this.rpcRequest('getblockhash', {
+      height: Number(height),
+    });
+  }
+
   // TODO: add returned data type
   rpcRequest(method: string, params?: any) {
-    const id = uuidv4();
-    Logger.debug(
-      `BitcoinCoreService.rpcRequest: method: ${method}, params: ${JSON.stringify(
-        params,
-      )}, id: ${id}`,
-    );
+    const payload = {
+      id: uuidv4(),
+      method,
+      params,
+      jsonrpc: '2.0',
+    };
+    Logger.debug(`BitcoinCoreService.rpcRequest: ${JSON.stringify(payload)}`);
     return this.httpService
-      .post(
-        this.configService.getOrThrow('BITCOIN_CORE_RPC_URL'),
-        {
-          jsonrpc: '2.0',
-          id,
-          method,
-          params,
+      .post(this.configService.getOrThrow('BITCOIN_CORE_RPC_URL'), payload, {
+        auth: {
+          username: this.configService.getOrThrow('BITCOIN_CORE_RPC_USERNAME'),
+          password: this.configService.getOrThrow('BITCOIN_CORE_RPC_PASSWORD'),
         },
-        {
-          auth: {
-            username: this.configService.getOrThrow(
-              'BITCOIN_CORE_RPC_USERNAME',
-            ),
-            password: this.configService.getOrThrow(
-              'BITCOIN_CORE_RPC_PASSWORD',
-            ),
-          },
-        },
-      )
-      .pipe(map((response) => response.data));
+      })
+      .pipe(
+        map((response) => {
+          Logger.debug(
+            `BitcoinCoreService.rpcRequest: ${JSON.stringify(response.data)}`,
+          );
+          return response.data.result;
+        }),
+        catchError((error) => {
+          Logger.error(
+            `BitcoinCoreService.rpcRequest: ${JSON.stringify(
+              error.response.data,
+            )}`,
+          );
+          throw new HttpException(
+            error.response.data.error.message,
+            error.response.status,
+          );
+        }),
+      );
   }
 }
--- src/blockchain.controller.ts
+++ src/blockchain.controller.ts
@@ -1,17 +1,52 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Logger,
+  Param,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Request, Response } from 'express';
 import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 import { BitcoinCoreService } from '../libs/bitcoin-core/src/bitcoin-core.service';
 
-@ApiTags("Blockchain")
-@Controller("/blockchain")
+@ApiTags('Blockchain')
+@Controller('/blockchain')
 export class BlockchainController {
-  constructor(
-    private readonly bitcoinCoreService: BitcoinCoreService,
-  ) {}
+  constructor(private readonly bitcoinCoreService: BitcoinCoreService) {}
 
-  @Get("/blockchainInfo")
-  getBlockchainInfo(): Observable<any> {
-    return this.bitcoinCoreService.getBlockchainInfo();
+  @Get('/blockchainInfo')
+  getBlockchainInfo(@Req() req: Request): Observable<any> {
+    return this.bitcoinCoreService.getBlockchainInfo().pipe(
+      map((response) => {
+        Logger.log(`${req.path}: ${JSON.stringify(response)}`);
+        return response;
+      }),
+      catchError((error) => {
+        Logger.error(`${req.path}: ${JSON.stringify(error.response)}`);
+        throw new HttpException(error.message, error.status);
+      }),
+    );
+  }
+
+  @Get('/blockHash/:blockHeight')
+  getBlockHash(
+    @Req() req: Request,
+    @Param('blockHeight') blockHeight: number,
+  ): Observable<any> {
+    return this.bitcoinCoreService.getBlockHash(blockHeight).pipe(
+      map((response) => {
+        Logger.log(`${req.path}: ${JSON.stringify(response)}`);
+        return response;
+      }),
+      catchError((error) => {
+        Logger.error(`${req.path}: ${JSON.stringify(error.response)}`);
+        throw new HttpException(error.message, error.status);
+      }),
+    );
   }
 }
